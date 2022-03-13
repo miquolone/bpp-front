0b98d932a28bfcec3c8926f90b343e9d7ca6576e
miquolone <miquolone@gmail.com>
2022-03-13 17:26:58 +0900

    nftをspreadsheetからの読み込みに対応

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ import ContactComponent from './pages/Contact';
 import OrganizeComponent from './pages/Organize';
 import { table, minifyRecords } from './lib/airtable';
 import Home from './pages/Home';
+import axios from 'axios';
 
 export const AirtableContext = createContext( {} );
+export const SpreadSheetContext = createContext( {} );
 
 function App() {
 
   const [ airtableData, setAirtableData ] = useState( {} );
+  const [ sheetData, setSheetData ] = useState( {} );
 
   useEffect( () => {
     table.select( {} ).all().then( async records => {
@@ -24,7 +27,16 @@ function App() {
     } );
   }, [ airtableData, setAirtableData ] );
 
+  useEffect( () => {
+    const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=iFYj1fCUGC6-vv7-hk7N7SX2Rxx55KVqH9xu83Hp3Bh6DYncvgW7opwQLTEQTifveAg9KtgJAnKrZOOYY83E-d8Fet36mGPLm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJm-9bVgU5_XYH0PPM-LaWgKhleizW24-4KsP1spuY4hSspXsvkzWXz-utb7v5P9IdRNO5Cap-uoKnkp-SZWtQ38vJJvcjLLM9z9Jw9Md8uu&lib=Mx9hv0ZJqejBvM1-inSXbfybYnTjGP0Z0';
+    axios.get( url ).then( res => {
+      console.log( res );
+      setSheetData( res.data );
+    } );
+  }, [ setSheetData, ] );
+
   return (
+    <SpreadSheetContext.Provider value={ sheetData}>
     <AirtableContext.Provider value={ airtableData }>
       <BrowserRouter>
         <Routes>
@@ -49,6 +61,7 @@ function App() {
         </Routes>
       </BrowserRouter>
     </AirtableContext.Provider>
+    </SpreadSheetContext.Provider>
   );
 }
 
