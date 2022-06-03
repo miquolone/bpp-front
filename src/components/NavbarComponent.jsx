23ff6ee758169f3639280df29f8534a91bb3fdce
miquolone <miquolone@gmail.com>
2022-06-04 00:34:24 +0900

    fetchpriorityを実験

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -5,7 +5,7 @@ const NavbarComponent = () => {
   return (
     <>
       <nav className="flex">
-        <a href={ "/" }> <img className="logo_img" src="/assets/images/bpp_150_alpha.png" alt={ "" }/> </a>
+        <a href={ "/" }> <img fetchpriority="height" className="logo_img" src="/assets/images/bpp_150_alpha.png" alt={ "" }/> </a>
         <div className={ "link" }>
           <Link to={ "/" }> HOME </Link>
           <Link to={ "/STORE" }> STORE </Link>
